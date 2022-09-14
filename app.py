638413ce81c66a0be0d5cfcc8dbb2ec49bd21ee4
fr3drikk <112628402+fr3drikk@users.noreply.github.com>
2022-09-14 16:02:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import numpy as np # Basic library for all kind of numerical operations
 import pandas as pd # Basic library for data manipulation in dataframes
 import matplotlib.pyplot as plt # comand for ploting
 import seaborn as sns; sns.set() # this is a data visualization library built on top of matplotlib
-%matplotlib inline
 from matplotlib.patches import ConnectionPatch # using this for later when zooming
 import plotly.express as px # Plotly plots
 
