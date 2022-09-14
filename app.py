8d488ab48dfc92413641059cf8af6e81546c9ff3
fr3drikk <112628402+fr3drikk@users.noreply.github.com>
2022-09-14 16:24:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,16 +13,12 @@ st.title('Shark attacks - GROUP 3 BABY')
 
 data = pd.read_csv('https://raw.githubusercontent.com/Alphambarushimana/Grup_3/main/attacks.csv', encoding='iso8859-1')
 
+tab1, tab2, tab3 = st.tabs(["Pie, Map, Line"])
 
-data.info()
 
 data.isna().sum()
 
 data.drop(['Case Number', 'Name', 'Injury', 'Time', 'Investigator or Source', 'pdf', 'href formula', 'href', 'Case Number.1', 'Case Number.2', 'original order', 'Unnamed: 22', 'Unnamed: 23'], axis = 1, inplace = True)
-
-data.info()
-
-data.columns
 
 # remove the space in the column name for better syntax and readability
 
@@ -49,15 +45,7 @@ fig.update_layout(height=500, width=600)
 st.plotly_chart(fig, use_container_width=True)
 
 
-#from 2010 - 2018 
-sex_attacks_2010_2018 = data[data.Year >= 2010].groupby('Gender')['Gender'].count()
-sex_attacks_2010_2018 = sex_attacks_2010_2018[(sex_attacks_2010_2018.index == 'Male') | (sex_attacks_2010_2018.index =='Female')| (sex_attacks_2010_2018.index=='Unknown')]
-sex_attacks
 
-fig = px.pie(sex_attacks_2010_2018, values=sex_attacks_2010_2018.values, names=sex_attacks_2010_2018.index, 
-             title='Shark Attacks by Gender - Between 2010-2018')
-fig.update_layout(height=500, width=600)
-fig.show()
 
 data.Year.unique()
 
