af8451fd8a41a94557bd206789987f0c09bd3356
fr3drikk <112628402+fr3drikk@users.noreply.github.com>
2022-09-14 16:02:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 
 import streamlit as st
+import numpy as np # Basic library for all kind of numerical operations
+import pandas as pd # Basic library for data manipulation in dataframes
+import matplotlib.pyplot as plt # comand for ploting
+import seaborn as sns; sns.set() # this is a data visualization library built on top of matplotlib
+%matplotlib inline
+from matplotlib.patches import ConnectionPatch # using this for later when zooming
+import plotly.express as px # Plotly plots
+
+
 
 st.title('Shark attacks - GROUP 3 BABY')
