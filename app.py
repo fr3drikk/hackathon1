3a0a250b411dfca103a2be19535b8a248a41bd31
fr3drikk <112628402+fr3drikk@users.noreply.github.com>
2022-09-15 11:25:41 +0200

    slider

--- app.py
+++ app.py
@@ -167,7 +167,8 @@ with tab3:
     st.header("Two Line Charts")
     st.plotly_chart(activity_fig, use_container_width=True)
     st.plotly_chart(year_fig, use_container_width=True)
-
+    x = st.slider("Year 1950-2018", value=50)
+    st.write("Slider year:", x)
 with tab4:
     st.header("Sharks In Action")
     st.image("https://www.gannett-cdn.com/presto/2021/07/14/NPOH/32c7c45d-5abc-49e5-aa81-71633454f748-greatwhiteshark.jpg?crop=4551,2560,x0,y421&width=3200&height=1801&format=pjpg&auto=webp")
